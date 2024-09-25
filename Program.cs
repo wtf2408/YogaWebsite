ffb0657cd9108b7c76bc75eddfeb736bfb3e1ec2
wtf2408 <bogdanevseyv@yandex.ru>
2024-09-25 09:00:18 +0300

    corrected the project

--- Program.cs
+++ Program.cs
@@ -1,12 +1,14 @@
+using Microsoft.AspNetCore.HttpLogging;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
- 
+
 app.UseDefaultFiles();  
-app.UseStaticFiles();   
+app.UseStaticFiles();
 
 app.MapPost("/signup", async (context) =>
 {
-    await Results.Json($"api work, {context.Request.Form["name"]}").ExecuteAsync(context);
+    await Results.Json($"API work, {context.Request.Form["name"]}").ExecuteAsync(context);
 });
 
 app.Run();
