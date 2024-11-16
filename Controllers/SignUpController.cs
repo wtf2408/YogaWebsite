c556614e88d7670b90e40b9ae2ca323652450d1c
wtf2408 <bogdanevseyv@yandex.ru>
2024-11-16 07:11:28 +0300

    added writing to file

--- Controllers/SignUpController.cs
+++ Controllers/SignUpController.cs
@@ -1,7 +1,7 @@
 using System.Diagnostics.Eventing.Reader;
 using Microsoft.AspNetCore.Mvc;
-using System.IO;
-using System.Text.Json;
+using Newtonsoft.Json;
+
 
 
 
@@ -9,24 +9,42 @@ namespace YogaWebsite.Controllers
 {
     public class SignUpController : Controller
     {
-        private string pathPossibleClinets;
+        private readonly string POSSIBLE_CLIENTS_PATH = @"/var/YogaMasterskyaStorage/possibleClients.json"; 
         private YogamasterskyaContext context;
         public SignUpController(YogamasterskyaContext context)
         {
             this.context = context;
+            
         }
         [HttpPost][Route("/signup")]
         public async Task FormHandler(string name, string phone, bool agree)
         {
-            if (agree && context.Database.CanConnect()) {
-                context.PossibleClients.Add(new PossibleClient()
+            if (agree)
+            {
+                string clientsJson;
+                List<PossibleClient> possibleClients;
+
+                using (FileStream fs = new FileStream(POSSIBLE_CLIENTS_PATH, FileMode.OpenOrCreate, FileAccess.ReadWrite))
+                using (StreamReader reader = new StreamReader(fs))
                 {
-                    Id = context.PossibleClients.ToList().Count == 0 ? 1 :
-                         context.PossibleClients.Select(pc => pc.Id).Max() + 1,
+                    clientsJson = await reader.ReadToEndAsync();
+                    possibleClients = JsonConvert.DeserializeObject<List<PossibleClient>>(clientsJson) ?? new List<PossibleClient>();
+                }
+
+                possibleClients.Add(new PossibleClient()
+                {
+                    Id = possibleClients.Count == 0 ? 1 :
+                        possibleClients.Select(pc => pc.Id).Max() + 1,
                     Name = name,
                     Phone = phone[0] == '+' ? phone : ("+7" + phone[1..])
                 });
-                context.SaveChanges();
+
+                using (FileStream fs = new FileStream(POSSIBLE_CLIENTS_PATH, FileMode.Truncate, FileAccess.Write, FileShare.None))
+                using (StreamWriter writer = new StreamWriter(fs))
+                {
+                    clientsJson = JsonConvert.SerializeObject(possibleClients);
+                    await writer.WriteAsync(clientsJson);
+                }
             }
         }
 
